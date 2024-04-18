62059d232f6e74e64c20bad4dc37dc7b62dffb76
wy9933 <583049572@qq.com>
2024-04-18 15:05:28 +0800

    delete test

--- testtest.py
+++
@@ -1,9 +0,0 @@
-from models.superpoint import SuperPoint
-import torch
-
-net = SuperPoint().cuda()
-net.train()
-points = torch.rand(1, 200, 3).cuda()  # 1 batch, 2 points, 3 coords
-p_feat, sp_atten, sp_feat, sp_param = net(points)
-loss = net.get_loss(points, p_feat, sp_atten, sp_feat, sp_param)
-print(loss)
